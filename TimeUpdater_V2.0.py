5df8cb5c0b9223d1813c1fae22b7a98c48cb935f
Nasir Sunny <63219166+NasirSunny50@users.noreply.github.com>
2022-11-14 11:20:43 +0600

    Function added

--- TimeUpdater_V2.0.py
+++ TimeUpdater_V2.0.py
@@ -1,50 +1,55 @@
+# import pyodbc
+# print(pyodbc.drivers())
+
 import sys
-import PySimpleGUI as sg
 import socket
 import pyodbc as odbc
 from datetime import datetime
-from tkinter import *
-from tkinter import messagebox
 
 # step 1
-try:
-    now = datetime.now()
-    cur_time = now.strftime('%m/%d/%Y %H:%M:%S.%f')
 
-    records = [
-        ['0007', cur_time[:23], cur_time[:23], None, 'Scheduler', None, None, None, cur_time[:23], 'Scheduler', 'N', '1']
+now = datetime.now()
+cur_time = now.strftime('%m/%d/%Y %H:%M:%S.%f')
+
+
+DRIVER = 'ODBC Driver 17 for SQL Server'
+SERVER_NAME = '192.168.1.10'
+DATABASE_NAME = 'CAAB_AFS_DB'
+USER = 'titcaab'
+PW = 'titcaab321'
+
+conn_string = f"""
+    Driver={{{DRIVER}}};
+    Server={SERVER_NAME};
+    Database={DATABASE_NAME};
+    UID={USER};
+    PWD={PW};
+"""
+
+try:
+    conn = odbc.connect(conn_string)
+except Exception:
+    print('Connection Error. Please Check Your Authorization')
+    # messagebox.showerror('TimeUpdater','Connection Error. Please Check Your Authorization!')
+    sys.exit()
+else:
+    print('Connected Successfully')
+    cursor = conn.cursor()
+
+
+def branch(value):
+    select_statement = """
+        SELECT LastOpenDate FROM UFN_SFS_GetLastOpenDateInfo(?)
+    """
+
+    select = [
+        [value]
     ]
 
-    DRIVER = 'SQL Server'
-    SERVER_NAME = '*******'
-    DATABASE_NAME = '******'
-    USER = '*******'
-    PW = '*******'
-
-    conn_string = f"""
-        Driver={{{DRIVER}}};
-        Server={SERVER_NAME};
-        Database={DATABASE_NAME};
-        UID={USER};
-        PWD={PW};
-    """
-
-    try:
-        conn = odbc.connect(conn_string)
-    except Exception:
-        print('Connection Error. Please Check Your Authorization')
-        messagebox.showerror('TimeUpdater','Connection Error. Please Check Your Authorization!')
-        sys.exit()
-    else:
-        cursor = conn.cursor()
-
-    select_statement = """
-        SELECT LastOpenDate FROM UFN_SFS_GetLastOpenDateInfo('0007')
-    """
-
-
-
-    cursor.execute(select_statement)
+    for i in select:
+        print(i)
+        cursor.execute(select_statement, i)
+    print("Successful")
 
     max_date = ''
     # Step 2
@@ -53,26 +58,32 @@ try:
         print(max_date)
 
     try:
-        update_statement ="""
-                UPDATE SFS_VALIDTRANSACTIONDATETIME SET VTDENDTIME=? WHERE VTDBRANCHCODE='0007' AND VTDTRANSDATE=?
+        update_statement = """
+                UPDATE SFS_VALIDTRANSACTIONDATETIME SET VTDENDTIME=? WHERE VTDBRANCHCODE=? AND VTDTRANSDATE=?
         """
         update = [
-            [cur_time[:23], max_date[:23]]
+            [cur_time[:23], value, max_date[:23]]
         ]
     except Exception:
-        messagebox.showerror('TimeUpdater','Date already exists!')
+        print('Date already exists!')
+        # messagebox.showerror('TimeUpdater','Date already exists!')
         sys.exit()
     else:
         for i in update:
             print(i)
             cursor.execute(update_statement, i[:23])
         print('records updated successfully')
-        messagebox.showinfo('TimeUpdater','Success!')
+        # messagebox.showinfo('TimeUpdater','Success!')
 
     insert_statement = """
         INSERT INTO SFS_VALIDTRANSACTIONDATETIME
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """
+
+    records = [
+        [value, cur_time[:23], cur_time[:23], None, 'Scheduler', None, None, None, cur_time[:23], 'Scheduler', 'N',
+         '1']
+    ]
     # Step 4
     for i in records:
         print(i)
@@ -80,11 +91,13 @@ try:
     print('records inserted successfully')
 
     cursor.commit()
-    cursor.close()
-except Exception:
-    messagebox.showerror('TimeUpdater','Date already exists!')
-    sys.exit()
 
+
+branch("0007")
+branch("0008")
+branch("0009")
+branch("0010")
+cursor.close()
 
 print('connection closed')
 conn.close()
